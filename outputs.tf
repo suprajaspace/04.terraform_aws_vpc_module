a2f3e54d5a1f752645755845ccfc3f2bd6562217
Supraja <supra556699@gmail.com>
2024-12-06 14:17:03 +0800

    terraform aws vpc custom module

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
-# output "vpc_id" {
-#     value = module.vpc.vpc_id
-# }
-
+output "vpc_id" {
+    value = module.vpc.vpc_id
+}
+output "public_subnet_ids" {
+    value = module.vpc.public_subnet_ids
+}
 # output "az_info" {
 #     value = module.vpc.az_info
 # }
--- provider.tf
+++ provider.tf
@@ -8,7 +8,7 @@ terraform {
 
   backend "s3" {
     bucket         = "supra-remote-state"
-    key            = "expense-vpc"
+    key            = "expense-vpcc"
     region         = "us-east-1"
     dynamodb_table = "supra-locking"
   }
